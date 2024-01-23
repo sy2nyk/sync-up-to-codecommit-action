6c3d9ee011e141a8b281fe53adcb10b5e97b4e9b
Valen-C12 <123928892+Valen-C12@users.noreply.github.com>
2024-01-23 10:31:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com.cn'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath false
 git remote add sync ${CodeCommitUrl}
-git push sync --mirror
+git push -f sync
